52fd22e65fa115ffe6ffbf735c3131a490a5ae4c
Baiboning <2523581400@qq.com>
2025-08-19 15:11:57 +0800

    counter

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { computed, ref } from 'vue'
+const count = ref(0)
+const double = computed(() => count.value * 2)
+
+function inc() {
+  count.value++
+}
+function dec() {
+  count.value--
+}
+function reset() {
+  count.value = 0
+}
+</script>
+
+<template>
+  <div class="counter">
+    <button @click="dec">➖</button>
+    <span>{{ count }}</span>
+
+    <button @click="inc">➕</button>
+    <button @click="reset">Reset</button>
+    <p>Double:{{ double }}</p>
+    <div style="color: red">
+      {{ Date.now() }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.counter {
+  display: flex;
+  gap: 16px;
+  align-items: center;
+}
+button {
+  width: 80px;
+  height: 80px;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    hmr: {
+      overlay: true, // 让报错浮层更明显
+    },
+  },
 })
