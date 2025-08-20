0ebc9d30de5450b7b3ec53361223b94d5570bbfb
Baiboning <2523581400@qq.com>
2025-08-20 16:55:49 +0800

    again

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <script setup>
 import { ref } from 'vue'
 // import TodoList from './components/TodoList.vue'
-
-import TodoFooter from '@/components/todo/TodoFooter.vue'
-import TodoHeader from '@/components/todo/TodoHeader.vue'
-import TodoList from '@/components/todo/TodoList.vue'
 import { useTodo } from '@/composables/useTodo'
 const todo = useTodo()
 
@@ -21,21 +17,6 @@ const show = ref(true)
   <!-- <ThemeProvider>
     <DeepChild></DeepChild>
   </ThemeProvider> -->
-  <section class="todoapp">
-    <TodoHeader @add="todo.add" />
-    <TodoList
-      :todos="todo.filteredTodos.value"
-      @toggle="todo.toggle"
-      @remove="todo.remove"
-    />
-    <TodoFooter
-      :left="todo.leftCount"
-      :completed-count="todo.completedCount"
-      :filter="todo.filter"
-      @set-filter="todo.filter = $event"
-      @clear="todo.clearCompleted"
-    />
-  </section>
 </template>
 
 <style scoped>
@@ -44,11 +25,5 @@ body {
   background: #f5f5f5;
   margin: 0;
   padding: 40px;
-}
-.todoapp {
-  background: #fff;
-  max-width: 500px;
-  margin: auto;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
 }
 </style>
